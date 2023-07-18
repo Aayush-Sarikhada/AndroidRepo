83bc64632cec3c1c7150c856f793b4a2014dc12f
Aayush Sarikhada <aayush.s@simformsolutions.com>
2023-07-18 20:30:15 +0530

    Add Intent examples, activity and fragment examples

---
+++ app/src/main/java/com/example/android/activitiesandfragments/adapter/ImageListAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.android.activitiesandfragments.adapter
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import com.example.android.R
+
+
+class ImageListAdapter(private val context: Context, private val uriList: List<Uri>) :
+    ArrayAdapter<Uri>(context, 0, uriList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var currentItemView = convertView
+        if (currentItemView == null) {
+            currentItemView =
+                LayoutInflater.from(context).inflate(R.layout.row_image_view, parent, false)
+        }
+        val currentUri: Uri? = getItem(position)
+        val imgView = currentItemView!!.findViewById<ImageView>(R.id.imageView)
+        imgView.setImageURI(currentUri)
+
+        return currentItemView
+    }
+
+}
